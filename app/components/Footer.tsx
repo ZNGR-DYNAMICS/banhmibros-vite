0b7b2492730242800b86115d72152fb9948366c0
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-02-01 17:01:57 +0100

    style(footer): update styling for responsiveness and copyright text, add bottom-text in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from './Link';
 export default function Footer(){
 
     return (
-        <section className="relative flex flex-col p-16 bg-black-300 text-white-500">
+        <section className="relative flex flex-col p-8 md:p-16 bg-black-300 text-white-500">
             <div className="absolute top-0 left-0 h-0.5 w-full bg-gray-500"></div>
-            <div className="flex flex-row gap-32">
-                <div className="flex flex-col gap-1">
+            <div className="flex flex-row justify-between md:justify-normal md:gap-16 lg:gap-32">
+                <div className="hidden md:flex flex-col gap-1">
                     <p>Banh Mi Bros</p>
                     <p>Metzgergasse 15</p>
                     <p>9000 St.Gallen</p>
@@ -20,20 +20,28 @@ export default function Footer(){
                     <Link href="/location">Location</Link>
                     <Link href="/contact">Contact</Link>
                 </div>
-                <div className="flex flex-col gap-1">
+                <div className="flex flex-col gap-1 items-end md:items-start">
                     <Link href="/instagram">Instagram</Link>
                     <Link href="/tiktok">TikTok</Link>
                     <Link href="/facebook">FaceBook</Link>
                 </div>
-                <div className='ml-auto'>
+                <div className='hidden md:flex ml-auto text-gray-500'>
                     <a href="https://zngr-dynamics.ch">Site by <span className="font-medium text-white-500">ZNGR</span></a>
                 </div>
             </div>
-            <div className="flex flex-row py-16 gap-16">
+            <div className="flex flex-row py-16 gap-16 justify-between md:justify-normal">
                 <Link href="/legal" data-en="Imprint" data-de="Impressum">Imprint</Link>
                 <Link href="/legal" data-en="Privacy Policy" data-de="Datenschutz">Privacy Policy</Link>
             </div>
-            <Link href="/about">About Us</Link>
+            <div className='flex flex-row justify-between text-gray-500'>
+                <p>Banh Mi Bros © 2024</p>
+                <a href="https://zngr-dynamics.ch">Site by <span className="font-medium text-white-500">ZNGR</span></a>
+            </div>
+            <div>
+                <h1 className='pt-8 text-8xl xl:text-10xl circula-bold sm:circula-extrabold xl:circula-black text-black-900'>
+                    Banh Mi Bros
+                </h1>
+            </div>
         </section>
     )
 }
